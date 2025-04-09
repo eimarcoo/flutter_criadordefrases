afcfe3335d54fd250f327568892a0b3eed39f51d
eimarcoo <eimarcoo@outlook.com>
2025-04-08 21:22:14 -0300

    subir

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Gerador de Frases',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.green,
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(color: Colors.green),
+                child: Image.asset('images/imga.jpg', width: 300),
+              ),
+
+              Text(
+                'Frase do Dia',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                ),
+                child: Text('Gerar Frase', style: TextStyle(fontSize: 20)),
+              ),
+            ],
+          ),
         ),
       ),
     );
